f0f4105b709c39fde7644ee1f9defb1c701759b8
Mihai Moraru <moraru.mihai@hotmail.com>
2020-07-17 11:50:43 +0200

    changes for flake8

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,4 +1,3 @@
 def add(x, y):
     """Add two numbers together"""
     return x + y
-    
--- app/app/tests.py
+++ app/app/tests.py
@@ -7,4 +7,3 @@ class CalcTest(TestCase):
     def test_add_numbers(self):
         """Simple unit test for the addition of two numbers"""
         self.assertEquals(add(3, 8), 11)
-        
